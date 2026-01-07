31e94d35584535c10fd6a3124cc6e1760358aa25
cedar <1903239203@qq.com>
2026-01-07 17:58:27 +0800

    feat(平方权重、困难视角回填)

--- train.py
+++ train.py
@@ -71,6 +71,8 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
     
     # 每个视角的损失值
     loss_per_view = {}
+    # 每个视角的权重
+    loss_weight_per_view = {}
 
     progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
     first_iter += 1
@@ -166,7 +168,16 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         loss_per_view[view_name] = loss.item()
         # 计算当前所有视角的平均损失，计算权重
         per_view_loss_weight = loss.item() / (sum(loss_per_view.values()) / len(loss_per_view))
+        # 平方权重
+        per_view_loss_weight = per_view_loss_weight * per_view_loss_weight
+        loss_weight_per_view[view_name] = per_view_loss_weight
         loss *= per_view_loss_weight
+        
+        # TODO 回填困难视角
+        if per_view_loss_weight > 5:
+            viewpoint_stack.append(viewpoint_cam)
+            viewpoint_indices.append(vind)
+            print("回填困难视角", view_name)
 
         loss.backward()
 
@@ -234,7 +245,8 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
                 print("\n[ITER {}] Saving Checkpoint".format(iteration))
                 torch.save((gaussians.capture(), iteration), scene.model_path + "/chkpnt" + str(iteration) + ".pth")
     # 输出每个视角的损失值
-    print(loss_per_view)
+    print("loss_pre_view", loss_per_view)
+    print("loss_weight_per_view", loss_weight_per_view)
 
 def prepare_output_and_logger(args):    
     if not args.model_path:
